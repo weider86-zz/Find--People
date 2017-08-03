790a3dae94fecc84bf63a385d1f9975e9cd7ca54
Weider de Brito Lima <weider@Mac-mini-de-Weider.local>
2017-08-03 19:40:11 -0300

    commit

---
+++ app.js
@@ -0,0 +1,23 @@
+//app
+
+$('#btn-find-female').on('click', function(){
+  findPeople('female');
+});
+$('#btn-find-male').on('click', function(){
+  findPeople('male');
+});
+$('#btn-find-random').on('click', function(){
+  findPeople('random');
+});
+
+function findPeople(gender){
+  $.ajax({
+    url: 'https://randomuser.me/api/?gender=' + gender,
+    dataType: 'json',
+    success: function(data) {
+      document.getElementById('picture').src = data.results[0].picture.large
+      document.getElementById('firstName').textContent = data.results[0].name.first
+      document.getElementById('email').textContent = data.results[0].email
+    }
+  });
+}
